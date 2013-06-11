8d8e5e31d9d27697fa1425aa830d09ccb43dd778
adrianbarabino <adrian.barabino@gmail.com>
2013-06-10 22:23:02 -0300

    borrado los CSS sin sentido

--- plantillas/2013/cabecera.php
+++ plantillas/2013/cabecera.php
@@ -13,33 +13,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <!-- Adding "maximum-scale=1" fixes the Mobile Safari auto-zoom bug: http://filamentgroup.com/examples/iosScaleBug/ -->
     
- 
-    
-    <!-- superfish -->
-    <link rel="stylesheet" media="screen" href="/css/superfish.css" /> 
 
-    
-
-    <link rel="stylesheet" href="/js/prettyPhoto/css/prettyPhoto.css"  media="screen" />
-    <!-- ENDS prettyPhoto -->
-    
-    <!-- Plugin de direcciones (address) -->
-
-    <!-- poshytip -->
-    <link rel="stylesheet" href="/js/poshytip-1.1/src/tip-twitter/tip-twitter.css"  />
-    <link rel="stylesheet" href="/js/poshytip-1.1/src/tip-yellowsimple/tip-yellowsimple.css"  />
-    <!-- ENDS poshytip -->
     
     
     <!-- GOOGLE FONTS -->
     <link href='http://fonts.googleapis.com/css?family=Allan:700' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Oswald:700' rel='stylesheet' type='text/css'>
 
-    <!-- Flex Slider -->
-    <link rel="stylesheet" href="/css/flexslider.css" >
-    <!-- ENDS Flex Slider -->
-    
-    
     
     <!-- Lessgrid -->
     <link rel="stylesheet" media="all" href="/css/lessgrid.css"/>
